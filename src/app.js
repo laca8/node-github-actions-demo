c73664eb7f8b3b120e7b6bdca7a98c9b9189fa52
Ragab <89777423+laca8@users.noreply.github.com>
2026-08-23 10:23:52 +0300

    initial node application

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello from Node.js",
+    });
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "ok",
+    });
+});
+
+module.exports = app;
